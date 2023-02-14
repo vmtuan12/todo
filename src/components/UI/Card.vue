8cd200fc4eb7545e0f029361a5e27fd45ab752a3
vmtuan12 <21020664@vnu.edu.vn>
2023-02-14 16:49:53 +0700

    [draft] card appear animation

--- src/components/UI/Card.vue
+++ src/components/UI/Card.vue
@@ -4,17 +4,21 @@
 <template>
     <div class="item">
         <div class="details">
-            <div>
+            <div class="checked">
                 <slot name="checked"></slot>
             </div>
-            <div>
-                <h2>
-                    <slot name="heading"></slot>
-                </h2>
+            <div class="task">
+                <div class="title">
+                    <h2>
+                        <slot name="heading"></slot>
+                    </h2>
+                </div>
+                <div class="description">
+                    <h3>
+                        <slot name="info"></slot>
+                    </h3>
+                </div>
             </div>
-            <!-- <h5>
-                <slot name="info"></slot>
-            </h5> -->
             <div>
                 <slot name="button"></slot>
             </div>
@@ -24,7 +28,8 @@
 
 <style scoped>
     .item {
-        display: flex;
+        
+        align-items: center;
         border-radius: 10px;
         border: 1.5px solid black;
         margin: auto;
@@ -32,7 +37,21 @@
         background-color: whitesmoke;
         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.66);
         margin-top: 10px;
+        background-color: white;
+        animation: card-appear 1s ease-in-out forwards;
     }
+
+    @keyframes card-appear {
+    from {
+        opacity: 0;
+        transform: translateY(5rem);
+    }
+  
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
 
     .details {
         display: flex;
@@ -40,11 +59,21 @@
         align-items: center;
     }
 
+    .task {
+        display: flex;
+        flex-direction: column;
+        width: auto;
+    }
+    .title {
+        display: flex;
+        height: fit-content;
+    }
+    .description {
+        display: flex;
+    }
+
     h2 {
         font-weight: bold;
         font-size: 30px;
-    }
-    h5 {
-        font-size: 15px;
     }
 </style>
