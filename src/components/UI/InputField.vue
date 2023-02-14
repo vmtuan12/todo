451a4c5c0ec1340bbfaa9c53a90a994acbc27e54
vmtuan12 <21020664@vnu.edu.vn>
2023-02-14 17:18:05 +0700

    [draft] basic css

--- src/components/UI/InputField.vue
+++ src/components/UI/InputField.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div class="item">
-        <div class="inp">
+        <div class="input">
             <slot name="inputTitle"></slot>
             <slot name="inputDescription"></slot>
         </div>
@@ -26,5 +26,26 @@
         max-width: 600px;
         background-color: whitesmoke;
         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.66);
+        gap: 10px;
+        animation: card-appear 1s ease-in-out forwards;
     }
+
+    @keyframes card-appear {
+        from {
+            opacity: 0;
+            transform: translateY(1rem);
+        }
+    
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+    .input {
+        display: flex;
+        gap: 10px;
+    }
+
+
 </style>
