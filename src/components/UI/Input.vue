59b125f68f9d373421f722503555a6f7d3fee8a2
vmtuan12 <21020664@vnu.edu.vn>
2023-02-14 15:46:10 +0700

    [draft] custom input component

---
+++ src/components/UI/Input.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    props: ['modelValue', 'placeHolder'],
+    emits: ['update:modelValue'],
+    
+}
+</script>
+
+<template>
+    <input
+        :value="modelValue"
+        :placeholder="placeHolder"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+</template>
+
+<style scoped>
+    input {
+        border: 1.5px solid black;
+        border-radius: 8px;
+
+    }
+    input:focus {
+        background-color: antiquewhite;
+    }
+</style>
