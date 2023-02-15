d4942ec22fbb9b816bab01be4add5c5307d4984d
vmtuan12 <21020664@vnu.edu.vn>
2023-02-15 21:10:42 +0700

    [draft] add transition for title

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,22 @@ import TodoList from './components/Todo/TodoList.vue';
 </script>
 
 <template>
+    <div class="title">To-do List</div>
     <TodoList/>
 </template>
 
 <style scoped>
-    
+    .title {
+        color: aliceblue;
+        display: flex;
+        justify-content: center;
+        font-size: 40px;
+        text-shadow: 0 10px 10px black;
+        transition: font-size 2s;
+        width: fit-content;
+        margin: auto;
+    }
+    .title:hover {
+        font-size: 60px;
+    }
 </style>
