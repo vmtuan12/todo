51192603e932b399507c9ed4b1b3bfceb2e1b3b5
vmtuan12 <21020664@vnu.edu.vn>
2023-02-16 15:03:17 +0700

    [draft] error modal for empty input

---
+++ src/components/UI/ErrorModal.vue
@@ -0,0 +1,63 @@
+<script>
+    export default {
+        emits: ['modalOff'],
+        methods: {
+            turnOffModal() {
+                this.$emit('modalOff')
+            }
+        }
+    }
+</script>
+
+<script setup>
+import ModalBack from '../UI/ModalBack.vue'
+import Button from '../UI/Button.vue'
+
+</script>
+
+<template>
+    <ModalBack @clicked="turnOffModal"/>
+    <div class="modal">
+        <div class="msg">Error</div>
+        <div class="btn">
+            <Button @pressed="turnOffModal">OK</Button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .modal {
+        display: flex;
+        flex-direction: column;
+        width: 300px;
+        height: 200px;
+        border: 1.5px solid black;
+        border-radius: 12px;
+        background-color: aliceblue;
+        align-items: center;
+        justify-content: center;
+        position: fixed;
+        z-index: 999;
+        margin-top: 2rem;
+        animation: modal-appear 1s ease-in-out forwards;
+    }
+
+    @keyframes modal-appear {
+        from {
+            opacity: 0;
+            transform: scaleX(0.3);
+        }
+        to {
+            opacity: 1;
+            transform: scaleX(1);
+        }
+    }
+
+    .msg {
+        position: fixed;
+    }
+
+    .btn {
+        margin-top: 100px;
+    }
+</style>
---
+++ src/components/UI/ModalBack.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+        methods: {
+            onClickHandler() {
+                this.$emit('clicked')
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="modal-back" @click="onClickHandler">
+        
+    </div>
+</template>
+
+<style scoped>
+    .modal-back {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        z-index: 100;
+        background: rgba(0, 0, 0, 0.75);
+    }
+</style>
