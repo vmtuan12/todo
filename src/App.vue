53e0f21b4139dec80ee0215a47a0d7ca0c376b68
vmtuan12 <21020664@vnu.edu.vn>
2023-02-17 21:01:54 +0700

    [draft] add multi page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
-<script setup>
+<script>
 import TodoList from './components/Todo/TodoList.vue';
+import TodoList2 from './components/Todo/TodoList2.vue';
 
+const routes = {
+    '/': TodoList,
+    '/todo2': TodoList2
+}
 
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+        currentView() {
+            return routes[this.currentPath.slice(1) || '/'] || NotFound
+        }
+    },
+    mounted() {
+        window.addEventListener('hashchange', () => {
+		    this.currentPath = window.location.hash
+		})
+    }
+}
 </script>
 
 <template>
-    <div class="title">To-do List</div>
-    <TodoList/>
+    <div class="navbar">
+        <a href="#/">TodoList1</a>
+        <a href="#/todo2">TodoList2</a>
+    </div>
+    <component :is="currentView" />
 </template>
 
 <style scoped>
-    .title {
-        color: aliceblue;
+    .navbar {
+        background-color: rgb(63, 63, 63);
+        opacity: 0.5;
         display: flex;
-        justify-content: center;
-        font-size: 40px;
-        text-shadow: 0 10px 10px black;
-        transition: font-size 2s;
-        width: fit-content;
-        margin: auto;
+        justify-content: space-evenly;
+        align-items: center;
+
     }
-    .title:hover {
-        font-size: 60px;
+
+    a {
+        color: rgb(180, 180, 180);
+        font-size: 25px;
+        text-decoration: none;
+        transition: font-size 0.25s;
+    }
+
+    a:hover {
+        color: rgb(255, 255, 255);
+        font-size: 27px;
     }
 </style>
